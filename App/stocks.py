75d8453a8ce72f80ac2b16c397903b06d59d894a
Lucio-Bernal25 <lab382@georgetown.edu>
2024-11-13 18:06:29 -0500

    refactoring...

--- App/stocks.py
+++ App/stocks.py
@@ -8,11 +8,7 @@ from dotenv import load_dotenv
 from pandas import read_csv
 from plotly.express import line
 
-
-load_dotenv() # looks in the ".env" file for env vars
-
-API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
-
+from App.alpha_service import API_KEY
 
 def fetch_stocks_csv(symbol):
     request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&outputsize=full&datatype=csv"
